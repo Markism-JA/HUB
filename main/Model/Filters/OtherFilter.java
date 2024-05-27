86de9c54d385ed1830486917036111f1b44c53a9
Markism <154087765+Markism-JA@users.noreply.github.com>
2024-05-28 01:28:45 +0800

    Filters

---
+++ main/Model/Filters/OtherFilter.java
@@ -0,0 +1,26 @@
+package main.Model.Filters;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import main.Model.Components.*;
+public class OtherFilter {
+  //FilterByRam
+  public List<Ram> filterByRAMNeeds(List<Ram> rams, int ramNeeds) {
+    return rams.stream().filter(ram -> ram.getCapacity() == ramNeeds).collect(Collectors.toList());
+  }
+  
+  //FilterByFormFactorCase
+  public List<Case> filterByFormFactor(List<Case> cases, String formFactor) {
+    switch (formFactor) {
+      case "ATX":
+        return cases.stream().filter(Case -> Case.getFormFactor().equalsIgnoreCase("ATX")).collect(Collectors.toList());
+      case "Micro-ATX":
+        return cases.stream().filter(Case -> Case.getFormFactor().equalsIgnoreCase("Micro-ATX")).collect(Collectors.toList());
+      case "Mini-ITX":
+        return cases.stream().filter(Case -> Case.getFormFactor().equalsIgnoreCase("Mini-ITX")).collect(Collectors.toList()); 
+      default:
+        throw new IllegalArgumentException("Invalid formFactor");
+    }
+  }
+}
--- main/Model/Filters/PriceFilter.java
+++ main/Model/Filters/PriceFilter.java
@@ -1,15 +1,11 @@
-package main.Model;
+package main.Model.Filters;
 
 import main.Model.Components.*;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
-public class Filter {
-
-
-    //Note: Filtering by Price
-    public List<CPU> filterCPUByPrice(List<CPU> cpus, String priceRange) {
+public class PriceFilter {
+public List<CPU> filterCPUByPrice(List<CPU> cpus, String priceRange) {
         switch(priceRange) {
             case "High":
                 return cpus.stream().filter(cpu -> cpu.getPrice() >= 15000).collect(Collectors.toList());
@@ -18,6 +14,7 @@ public class Filter {
             case "Low":
                 return cpus.stream().filter(cpu -> cpu.getPrice() < 8000).collect(Collectors.toList());
         }
+        return cpus;
     }
 
     public List<GPU> filterGPUByPrice(List<GPU> gpus, String priceRange) {
@@ -29,6 +26,7 @@ public class Filter {
             case "Low":
                 return gpus.stream().filter(gpu -> gpu.getPrice() < 10000).collect(Collectors.toList());
         }
+        return gpus;
     }
 
     public List<SSD> filterSSDByPrice (List<SSD> ssds, String priceRange) {
@@ -40,6 +38,7 @@ public class Filter {
             case "Low":
                 return ssds.stream().filter(SSD -> SSD.getPrice() < 2500).collect(Collectors.toList());
         }
+        return ssds;
     }
 
     public List<HDD> filterHddByPrice (List<HDD> hdds, String priceRange) {
@@ -51,6 +50,7 @@ public class Filter {
             case "Low":
                 return hdds.stream().filter(HDD -> HDD.getPrice() < 3000).collect(Collectors.toList());
         }
+        return hdds;
     }
 
     public List<MotherBoard> filterMotherBoardsByPrice (List<MotherBoard> motherBoards, String priceRance) {
@@ -62,6 +62,7 @@ public class Filter {
             case "Low":
                 return motherBoards.stream().filter(motherBoard -> motherBoard.getPrice() < 3500).collect(Collectors.toList());
         }
+        return motherBoards;
     }
 
     public List<InternalStorage> filterInternalStorageByPrice (List<InternalStorage> internalStorages, String priceRange) {
@@ -73,6 +74,7 @@ public class Filter {
             case "Low":
                 return internalStorages.stream().filter(internalStorage -> internalStorage.getPrice() < 3500).collect(Collectors.toList());
         }
+        return internalStorages;
     }
 
     public List<Ram> filterRamByPrice(List<Ram> rams, String priceRange) {
@@ -84,6 +86,7 @@ public class Filter {
             case "Low":
                 return rams.stream().filter(cpu -> cpu.getPrice() < 2500).collect(Collectors.toList());
             }
+        return rams;
     }
 
     public List<PSU> filterPSUByPrice(List<PSU> psus, String priceRange) {
@@ -95,6 +98,7 @@ public class Filter {
             case "Low":
                 return psus.stream().filter(PSU -> PSU.getPrice() < 2000).collect(Collectors.toList());
         }
+        return psus;
     }
 
     public List<Case> filterCaseByPrice(List<Case> cases, String priceRange) {
@@ -106,6 +110,7 @@ public class Filter {
             case "Low":
                 return cases.stream().filter(Case -> Case.getPrice() < 2000).collect(Collectors.toList());
         }
+        return cases;
     }
 
     public List<Fan> filterFansByPrice(List<Fan> fans, String priceRange) {
@@ -118,74 +123,6 @@ public class Filter {
             return fans.stream().filter(Fan -> Fan.getPrice() < 1500).collect(Collectors.toList());
 
         }
-    }
-
-
-    //FilterByPurpose
-    public List<CPU> filterCPUsByPurpose(List<CPU> cpus, String purpose) {
-        switch (purpose) {
-            case "Gaming":
-                return cpus.stream().filter(cpu -> cpu.getClockSpeed() >= 3.5 && cpu.getCoreCount() >= 4).collect(Collectors.toList());
-            case "Workstation":
-                return cpus.stream().filter(cpu -> cpu.getCoreCount() >= 8).collect(Collectors.toList());
-            case "General Use":
-                return cpus.stream().filter(cpu -> cpu.getClockSpeed() >= 2.0).collect(Collectors.toList());
-            default:
-                throw new IllegalArgumentException("Invalid primary purpose: " + purpose);
-        }
-    }
-
-    public List<GPU> filterGPUsByPurpose(List<GPU> gpus, String purpose) {
-        switch (purpose) {
-            case "Gaming":
-                return gpus.stream().filter(gpu -> gpu.getMemory() >= 4).collect(Collectors.toList());
-            case "Workstation":
-                return gpus.stream().filter(gpu -> gpu.getMemory() >= 8).collect(Collectors.toList());
-            case "General Use":
-                return gpus.stream().filter(gpu -> gpu.getPrice() <= 8000).collect(Collectors.toList());
-            default:
-                throw new IllegalArgumentException("Invalid primary purpose: " + purpose);
-        }
-    }
-
-    public List<Ram> filterRAMsByPurpose(List<Ram> rams, String purpose) {
-        switch (purpose) {
-            case "Gaming":
-                return rams.stream().filter(ram -> ram.getCapacity() >= 16).collect(Collectors.toList());
-            case "Workstation":
-                return rams.stream().filter(ram -> ram.getCapacity() >= 32).collect(Collectors.toList());
-            case "General Use":
-                return rams.stream().filter(ram -> ram.getCapacity() >= 8).collect(Collectors.toList());
-            default:
-                throw new IllegalArgumentException("Invalid primary purpose: " + purpose);
-        }
-    }
-
-    public List<InternalStorage> filterStoragesByPurpose(List<InternalStorage> storages, String purpose) {
-        switch (purpose) {
-            case "Gaming":
-                return storages.stream().filter(storage -> storage.getStorageSize() >= 512).collect(Collectors.toList());
-            case "Workstation":
-                return storages.stream().filter(storage -> storage.getType().equalsIgnoreCase("SSD")).collect(Collectors.toList());
-            case "General Use":
-                return storages.stream().filter(storage -> storage.getType().equalsIgnoreCase("SSD") || storage.getType().equalsIgnoreCase("HDD")).collect(Collectors.toList());
-            default:
-                throw new IllegalArgumentException("Invalid primary purpose: " + purpose);
-        }
-    }
-
-    public List<Component> filterByBrand(List<Component> components, String brand) {
-        if (brand.equalsIgnoreCase("No Preference")) {
-            return components;
-        }
-        return components.stream().filter(component -> component.getBrand().equalsIgnoreCase(brand)).collect(Collectors.toList());
-    }
-
-    public List<InternalStorage> filterByStorageNeeds(List<InternalStorage> storages, int storageNeeds) {
-        return storages.stream().filter(storage -> storage.getCapacity() == storageNeeds).collect(Collectors.toList());
-    }
-
-    public List<Ram> filterByRAMNeeds(List<Ram> rams, int ramNeeds) {
-        return rams.stream().filter(ram -> ram.getCapacity() == ramNeeds).collect(Collectors.toList());
+        return fans;
     }
 }
