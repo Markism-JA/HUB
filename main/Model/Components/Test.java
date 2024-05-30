32cb5724e02aa823f7aaaeb084fd33b6e74aea26
Markism <154087765+Markism-JA@users.noreply.github.com>
2024-05-31 01:09:08 +0800

    FIxed Compatibility Checker & Recommendation

--- main/Model/Components/Test.java
+++ main/Model/Components/Test.java
@@ -1,16 +1,13 @@
 package main.Model.Components;
 
-import main.Model.Components.*;
+import main.Model.Components.CPU;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.util.Scanner;
+
 public class Test {
   public static void main(String[] args) {
-    String fileName = "/home/marky/Documents/GitHub/HUB/main/Resources/data/cpu.csv";
-    List<CPU> readCPUs = CPU.readCPUFromCSV(fileName);
-
-    for (CPU cpu : readCPUs) {
-      System.out.println(cpu.toString());
-    }
+    
   }
 }
--- main/Model/Filters/PurposeFilter.java
+++ main/Model/Filters/PurposeFilter.java
@@ -13,7 +13,7 @@ public class PurposeFilter {
                 return cpus.stream().filter(cpu -> cpu.getClockSpeed() >= 3.5 && cpu.getCoreCount() >= 4).collect(Collectors.toList());
             case "Workstation":
                 return cpus.stream().filter(cpu -> cpu.getCoreCount() >= 8).collect(Collectors.toList());
-            case "General Use":
+            case "General":
                 return cpus.stream().filter(cpu -> cpu.getClockSpeed() >= 2.0).collect(Collectors.toList());
             default:
                 throw new IllegalArgumentException("Invalid primary purpose: " + purpose);
@@ -26,7 +26,7 @@ public class PurposeFilter {
               return gpus.stream().filter(gpu -> gpu.getMemory() >= 4).collect(Collectors.toList());
           case "Workstation":
               return gpus.stream().filter(gpu -> gpu.getMemory() >= 8).collect(Collectors.toList());
-          case "General Use":
+          case "General":
               return gpus.stream().filter(gpu -> gpu.getPrice() <= 8000).collect(Collectors.toList());
           default:
               throw new IllegalArgumentException("Invalid primary purpose: " + purpose);
@@ -39,7 +39,7 @@ public class PurposeFilter {
               return rams.stream().filter(ram -> ram.getCapacity() >= 16).collect(Collectors.toList());
           case "Workstation":
               return rams.stream().filter(ram -> ram.getCapacity() >= 32).collect(Collectors.toList());
-          case "General Use":
+          case "General":
               return rams.stream().filter(ram -> ram.getCapacity() >= 8).collect(Collectors.toList());
           default:
               throw new IllegalArgumentException("Invalid primary purpose: " + purpose);
