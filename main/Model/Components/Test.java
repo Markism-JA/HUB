17f04f6022d65645dc33afe73e71fcbd99520327
Doffy777 <ronsherwinbugarin@gmail.com>
2024-06-01 01:34:16 +0800

    fix

--- main/Model/Components/Test.java
+++ main/Model/Components/Test.java
@@ -1,5 +1,6 @@
 package main.Model.Components;
-
+import main.Resources.*;
+import main.Model.Components.*;
 import main.Model.Components.CPU;
 import java.util.List;
 import java.util.ArrayList;
@@ -8,6 +9,12 @@ import java.util.Scanner;
 
 public class Test {
   public static void main(String[] args) {
+    String fileName = "main/Resources/data/case.csv";
+    List<Fan> readCPUs = Fan.readFanFromCSV(fileName);
+
+    for (Fan cpu : readCPUs) {
+      System.out.println(cpu.toString());
+    }
     
   }
 }
