3e6fa04f0ece71b29de3597ce342ebcffd3c3b94
Doffy777 <ronsherwinbugarin@gmail.com>
2024-06-06 02:10:56 +0800

    test

--- main/View/Front/SignUpController.java
+++ main/View/Front/SignUpController.java
@@ -5,28 +5,40 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import main.Model.DataManager;
 import main.Model.UserAccountManagement;
 import main.Model.UserPreferences;
 
 public class SignUpController {
     private MainApp mainApp;
     private UserAccountManagement userAccountManagement;
+    private DataManager dataManager;
+
+
+    public void setDataManager(DataManager dataManager) {
+        this.dataManager = dataManager;
+    }
 
     @FXML
     private Label errorLabel;
+
+    @FXML
+    private Label emptyLabel;
 
     public void setMainApp(MainApp mainApp) {
         this.mainApp = mainApp;
     }
 
-    public void setUserAccountManagement(UserAccountManagement userAccountManagement) {
-        this.userAccountManagement = userAccountManagement;
+    public void setUserAccountManagement() {
+        this.userAccountManagement = new UserAccountManagement(dataManager.getUsers());
     }
+
 
     @FXML
     private TextField usernameField;
@@ -49,23 +61,28 @@ public class SignUpController {
         // Get the current stage from the signUpButton
         Stage stage = (Stage) signUpButton.getScene().getWindow();
         Parent root = null;
-    
-        // Create default preferences for the user
-        // UserPreferences preferences = new UserPreferences(null, null, null, null, 0, 0, null);
+        
     
         if (!password.equals(verifyPassword)) {
             errorLabel.setText("Passwords do not match. Please re-enter your password.");
 
         } else if (password.equals("") && passVerify.equals("")) {
             errorLabel.setText("Please enter a password.");
+           
+        
+        } else if ( username.equals("")) {
+            emptyLabel.setText("Please enter a username.");
             
         } else if (password.equals(verifyPassword) && !password.equals("")) {
 
             // Clear the error message if registration is successful
             errorLabel.setText("");
+            emptyLabel.setText("");
             
+            setUserAccountManagement();
+
             // Register the user
-            // userAccountManagement.registerUser(username, password, preferences);
+            userAccountManagement.registerUser(username, password);
 
             try {
                 root = FXMLLoader.load(getClass().getResource("FrontUserPreference.fxml"));
